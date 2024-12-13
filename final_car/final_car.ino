0b6b2364b6606d93ab3ab95035595f805f168428
Kappaslab <acounts@kappas.work>
2024-12-13 14:50:26 +0900

    update:update

--- final_car/final_car.ino
+++ final_car/final_car.ino
@@ -16,20 +16,22 @@
 
 /*定数*/
 #define PI 3.141593
-#define TIER_DIAMETER 35 //[mm]
+#define TIER_DIAMETER 37 //[mm]
 #define ROBOT_WIDTH 104 //[mm]
 #define ENC_SLIT 40
-#define INTERRUPT_FREQ 20//[Hz]
+#define INTERRUPT_FREQ 100//[Hz]
 #define DIRECTION_MAX 1
 #define VELOCITY_MAX 100//[mm/s]
-
-
-FspTimer time_interrupt;
 
 /* Wi-Fi 設定 */
 const char* ssid = "cafe_03";
 const char* password = "123456789";
-WiFiServer server(80);
+byte IP* = { 192, 48, 56, 1 };
+int PORT = 80;
+int status = WL_IDLE_STATUS;
+WiFiServer server(PORT);
+
+FspTimer time_interrupt;
 
 /*value*/
 typedef struct enc_str{
@@ -94,7 +96,7 @@ void setup() {
     /*シリアル通信*/
     Serial.begin(9600);
     /*Wi-Fi通信*/
-    WiFi.config(IPAddress(192, 48, 56, 1));
+    WiFi.config(IPAddress(IP));
     WiFi.beginAP(ssid, password);
 
     while (WiFi.status() != WL_AP_LISTENING) {
@@ -109,4 +111,82 @@ void setup() {
 
 void loop(){
 
+}
+
+void enc_counter_L(){
+    enc[0].moved = true;
+}
+
+void enc_counter_R(){
+    flag_R = true;
+}
+
+void timer_callback(timer_callback_args_t *arg){
+    float enc_diff[2];
+    float radius;
+    float global_theta;
+    float local_theta;
+    float local_x;
+    float local_y;
+    static int prev_enc[2];
+    static int counter = 0;
+
+    if(flag_L){
+        if(enc[0].rotate_forward){
+            enc[0].count ++;
+        }else{
+            enc[0].count --;
+        }
+        flag_L = false;
+    }
+    
+    if(flag_R){
+        if(enc[1].rotate_forward){
+            enc[1].count ++;
+        }else{
+            enc[1].count --;
+        }
+        flag_R = false;
+    }
+    counter++;
+    if(counter == 2){
+        digitalWrite(TEST_PIN, HIGH);
+        counter = 0;
+        /*前回からの移動量*/
+        enc_diff[0] = (enc[0].count - prev_enc[0]);
+        enc_diff[1] = (enc[1].count - prev_enc[1]);
+        /*エンコーダデータの更新*/
+        prev_enc[0] = enc[0].count;
+        prev_enc[1] = enc[1].count;
+
+        /*割り込みを許可*/
+        interrupts();
+
+        /*エンコーダのカウントを移動距離[mm]に変換*/
+        enc_diff[0] = TIER_DIAMETER * PI * enc_diff[0] / ENC_SLIT;
+        enc_diff[1] = TIER_DIAMETER * PI * enc_diff[1] / ENC_SLIT;
+
+        /*移動距離を位置と方向に変換*/
+        local_theta = (enc_diff[0] - enc_diff[1])/ ROBOT_WIDTH;
+        if(local_theta > 360) local_theta -= 360;
+    
+        if(enc_diff[0] == enc_diff[1]){
+            robot.y += enc_diff[0];
+        }else{
+            radius = ROBOT_WIDTH * (enc_diff[0] + enc_diff[1]) / (enc_diff[0] - enc_diff[1]);
+            rad1 = radius;
+            global_theta = robot.headding * 2 * PI / 360;
+            local_x = radius * (1 - cos(local_theta)) / 4 ; //何故か４で割らねばならない
+            local_y = radius * sin(local_theta) / 4 ; //何故か４で割らねばならない
+            robot.x += local_y * sin(global_theta) + local_x * cos(global_theta);
+            robot.y += local_y * cos(global_theta) - local_x * sin(global_theta);
+        }
+        robot.headding += 360 * local_theta / (2 * PI * 1.25); //何故か1.25で割らねばならない
+        if(robot.headding > 360) robot.headding -= 360;
+
+        /*速度情報の更新*/
+        robot.v_L = enc_diff[0] * 10;
+        robot.v_R = enc_diff[1] * 10;
+        robot.v = (robot.v_L + robot.v_R) * 10 / 2;
+    }
 }
